28fecc6c1a6c2457bf3f9c192d83c3e4368b15a0
ludin-lee <alsdyd0331@naver.com>
2022-10-28 19:36:02 +0900

    final (JY&MY)
    gg

--- app.py
+++ app.py
@@ -120,5 +120,49 @@ def view_count_post():
     return jsonify('msg',"view-count+1 완료")
 
 
+# ---------------------------
+from datetime import datetime
+now = datetime.now()
+import certifi
+
+ca = certifi.where()
+
+@app.route('/article')
+def article():
+    return render_template('article.html')
+
+@app.route("/article/post", methods=["POST"])
+def web_article_post():
+    title_receive = request.form['title_give']
+    name_receive = request.form['name_give']
+    tag_receive = request.form['tag_give']
+    content_receive = request.form['content_give']
+    article_list = list(db.article.find({},{'_id':False}))
+    count = len(article_list) + 1
+    now = datetime.now()
+    current_time = now.strftime("%Y/%m/%d, %H:%M:%S")
+    view_count = 0
+    comment_count = 0
+
+
+    doc ={
+        'title':title_receive,
+        'nickname':name_receive,
+        'tag':tag_receive,
+        'content':content_receive,
+        'post_num': count,
+        'time':current_time,
+        'view_count': view_count,
+        'comment_count': comment_count
+    }
+
+    db.article.insert_one(doc)
+    return jsonify({'msg': '등록완료!'})
+
+@app.route("/article", methods=["GET"])
+def web_article_get():
+    return jsonify({'articles': 'GET 연결 완료!'})
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

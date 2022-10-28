cdc5ac4aa11065363dbd7ce2d94bb4416983e4c6
kimjin2 <103062714+kimjin2@users.noreply.github.com>
2022-10-28 19:22:22 +0900

    최종(2)

---
+++ article.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from datetime import datetime
+now = datetime.now()
+
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.cctcpnr.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.guhaejo
+
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+@app.route('/')
+def home():
+    return render_template('article.html')
+
+@app.route("/article", methods=["POST"])
+def web_article_post():
+    title_receive = request.form['title_give']
+    name_receive = request.form['name_give']
+    tag_receive = request.form['tag_give']
+    content_receive = request.form['content_give']
+    article_list = list(db.article.find({},{'_id':False}))
+    count = len(article_list) + 1
+    now = datetime.now()
+    current_time = now.strftime("%Y/%m/%d, %H:%M:%S")
+    view_count = 0
+    comment_count = 0
+
+
+    doc ={
+        'title':title_receive,
+        'nickname':name_receive,
+        'tag':tag_receive,
+        'content':content_receive,
+        'post_num': count,
+        'time':current_time,
+        'view_count': view_count,
+        'comment_count': comment_count
+    }
+
+    db.article.insert_one(doc)
+    return jsonify({'msg': '등록완료!'})
+
+@app.route("/article", methods=["GET"])
+def web_article_get():
+
+    return jsonify({'articles': 'GET 연결 완료!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
